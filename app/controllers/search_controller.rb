25db06823d97233c36f37b77f434156dffa57cf4
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-15 09:59:56 -0700

    how do i find 100ish people that live in fire nation?

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,10 +4,9 @@ class SearchController < ApplicationController
   def index
     nation = params[:nation]
     conn = Faraday.new(url: "https://last-airbender-api.fly.dev")
-
+    require 'pry'; binding.pry
     response = conn.get("/api/v1/characters/?affiliation=#{nation}")
     json = JSON.parse(response.body, symbolize_names: true)
     @members = json
-
   end
 end
