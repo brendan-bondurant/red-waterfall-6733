2f04567d1019b17e22a6a41afd196f2cb587ec57
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-15 10:49:36 -0700

    test for image

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -23,8 +23,9 @@ RSpec.describe 'welcome page', type: :feature do
     expect(current_path).to eq(search_path)
     expect(page).to have_content("Total Members = 97")
     expect(page).to have_content("Name: Azula")
-    expect(page).to have_content("Allies")
-    expect(page).to have_content("Enemies")
-    save_and_open_page
+    expect(page).to have_content("Allies: Ozai, Zuko")
+    expect(page).to have_content("Enemies: Iroh, Zuko, Kuei, Long Feng, Mai, Ty Lee, Ursa")
+    expect(page).to have_content("Affiliations: Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
+    expect(page).to have_css('img[src="https://vignette.wikia.nocookie.net/avatar/images/1/12/Azula.png/revision/latest?cb=20140905084941"]')
   end
 end
