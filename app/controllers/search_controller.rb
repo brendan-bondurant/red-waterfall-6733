12bcd21bc7516e440adffe9d92b62d8166c672d0
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-12-15 10:38:18 -0700

    cleaned up allies and enemies

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,7 +9,26 @@ class SearchController < ApplicationController
     # require 'pry'; binding.pry
     response = conn.get("/api/v1/characters?perPage=100&affiliation=#{nation}")
     json = JSON.parse(response.body, symbolize_names: true)
-    @members = json
-    # require 'pry'; binding.pry
+    @members = json.map do |member|
+      member[:allies] = allies(member[:allies])
+      member[:enemies] = enemies(member[:enemies])
+      member
+    end
+  end
+  private
+
+  def allies(allies)
+    if allies != []
+      allies.join(', ')
+    else
+      return "None"
+    end
+  end
+  def enemies(enemies)
+    if enemies != []
+      enemies.join(', ')
+    else
+      return "None"
+    end
   end
 end
